0d90c9ed83c14708fcd669b93e9fd84d95b797d9
Filipe Leoni <filipe_sempre@outlook.com>
2023-07-20 11:05:12 -0300

    feat: Cors added

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,17 @@ import cookieParser from 'cookie-parser';
 import usersRoutes from './http/controllers/users/routes';
 import animesRoutes from './http/controllers/animes/routes';
 import episodesRoutes from './http/controllers/episodes/routes';
+const cors = require('cors');
 
 export const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
