42caa04f960225cecd8c3d04f124a2edd55e5114
Filipe Leoni <filipe_sempre@outlook.com>
2023-08-14 10:13:45 -0300

    fix: Cors config

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,20 @@ import usersRoutes from './http/controllers/users/routes';
 import animesRoutes from './http/controllers/animes/routes';
 
 export const app = express();
+var cors = require('cors');
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, PATCH');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-  );
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+app.use(cors());
+
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, PATCH');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+//   );
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
