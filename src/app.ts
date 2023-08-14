4ed87d6f9c4e70a978f2f19664e8fb6277b747b5
Filipe Leoni <filipe_sempre@outlook.com>
2023-08-14 10:20:24 -0300

    fix: Cors config

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,28 @@ import usersRoutes from './http/controllers/users/routes';
 import animesRoutes from './http/controllers/animes/routes';
 
 export const app = express();
-var cors = require('cors');
 
-app.use(cors());
+app.use(function (req, res, next) {
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://anime-zeta-rho.vercel.app',
+  ];
 
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, PATCH');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-//   );
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
+  const origin: any = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, PATCH');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
