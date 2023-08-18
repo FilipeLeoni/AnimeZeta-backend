69a057111f2472b2c5958e58672fb10efddf8c3c
Filipe Leoni <filipe_sempre@outlook.com>
2023-08-18 08:23:27 -0300

    fix: Avatar Dirname

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use(usersRoutes);
 app.use(animesRoutes);
